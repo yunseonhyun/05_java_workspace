93e8576d293cb1c0351c6a4533e4529c8c30d42d
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-08-25 13:19:59 +0900

    변수

---
+++ src/VariableEx1.java
@@ -0,0 +1,109 @@
+public class VariableEx1 {
+    public static void main(String[] args) {
+
+        // 변수 : 메모리에 값을 저장하기 위한 공간의 명칭
+        // 이 때, 저장되는 값이 변할 수 있기 때문에 변수
+
+        // 자료형 (data Type)
+        // - 변수 또는 값의 크기와 형식
+
+        /* ***** 자바 기본 자료형(Java Primitive Types) ***
+
+
+        [논리형]
+        boolean (1type) - true / false
+
+        [정수형]
+        byte    (1byte)
+        short   (2byte)
+        int     (4byte)     * 정수의 기본형 *
+        long    (8byte)     - 숫자 뒤에 L 작성 소문자 가능
+
+        [실수형]
+        float   (4byte)     - 숫자 뒤에 f
+        double  (8byte)     * 실수의 기본형 *
+
+        [문자형]
+        char    (2byte)     - 문자 하나, 문자 양 옆에 '' 작은 따옴표 사용
+
+          */
+
+        // 변수 선언 : 메모리에 공간 할당
+
+        boolean isTrue; // 메모리에 bollean 저장 공간을 1byte 할당하고
+                        // 공간의 명칭을 isTrue라고 부르겠다. 설정
+
+        double number1; // 메모리에 double 저장 공간을 8byte 할당하고
+                        // 그 공간을 number1 이라고 부르겠다. 설정
+
+        // 값 대입 : 변수 이름에 특정 값을 담아놓는 것
+        //              (이미 값이 존재하는 변수 이름에 대입하면 --> 덮어쓰기가 됨)
+
+        isTrue = true; // null 빈 공간값에 true라는 명칭으로 덮어쓰기
+
+        // sout + tab = System.out.println() 자동완성
+        // so + tab = System.out.printf("") 자동완성
+
+        System.out.println("isTrue : " + isTrue); // true
+
+        isTrue = false;
+
+        System.out.println("isTrue : " + isTrue); // false로 덮어쓰기 되는 것을 확인할 수 있음
+
+        // 변수선언 + 초기화 + 값의 범위 확인
+        int number2 = 2100000000; // 21억 오류 X
+        //  숫자 자료형들은   리터럴 값이 기본적으로 int이기 때문
+        // byte short int long은 모두 값(=리터럴)에서 뒤에 자료형 명칭이 붙지 않는한 모두 int
+        // long number3 = 2200000000; // int 22억 오류 O integer 숫자가 너무 큽니다
+
+        // 리터럴 값을 int -> Long의 값으로 변환
+        long number3 = 2200000000L; // int 22억 오류 X
+
+        /** 자바 기본 변수 선언 문법
+         * 자료형 변수이름 = 리터럴;
+         *
+         * 자료형(Data Type) : 변수가 저장할 데이터의 종류를 지정
+         * 변수이름(Variable Name) : 데이터를 저장할 메모리 공간의 이름
+         * 리터럴(Literal) : 실제 저장되는 값 자체
+         *
+         * 리터럴과 값의 차이
+         *
+         * 리터럴(literal) : 코딩에서 직접적으로 작성된 고정된 값
+         * 값 (value) : 변수나 표현식이 실제로 가지고 있는 데이터
+         *
+         * int a = 100; // 리터럴이자 값
+         * int b = a + 50; // 리터럴이 아니고 값
+         * int c = method명칭(); // 리터럴이 아니고 메서드 리턴값
+         * int d = Scanner.nextInt(); // 리터럴이 아니고 사용자의 입력값
+         * 리터럴은 값 내부에 존재하는 종류 중 하나
+         *
+         * 직접적으로 값이 정짖하게 작성되어 있지 않은 모든 것은 리터럴이 아님
+         *
+         */
+
+        System.out.println("number2 : " + number2);
+        System.out.println("number3 : " + number3);
+
+        /*
+        * nt1, nt2 는 4byte 크기의 데이터를 담는 바구니 명칭
+        *             int 이하의 크기의 데이터를 담을 수 있음
+        *             그런데 nt2에 2배가량이 넘는 long 데이터를 담아 넣으려 하기 때문에
+        *             바구니 크기에 비하여 데이터가 크다는 문제가 발생
+        *
+        *           값을 억지로 넣을 수는 있지만 값이 왜곡되어 변수이름 바구니에 담김
+        *           => 강제 형변환
+        *               숫자나 문자 구문 앞에 바구니 자료형을 소괄호로 작성하면 형변환
+        *
+        * */
+        int nt1 = 100;
+        int nt2 = (int)100000000000L; // long 데이터가 int로 강제 형변환 데이터 변질
+
+        long lng1 = 200;    // int 리터럴을 2배 가량의 메모리를 차지하는 Long에 담을 수 있음
+                            // 큰 바구니에 작은 데이터를 담는 것은 문제가 되지 않음
+                            // long과 같이 리터럴에 L을 작성하는 경우에는
+                            // 자료형 타입에도 Long 작성할 사소한 문제를 발생할 수 있음
+        long lng2 = 20000000000L;   // 기본 자료형은 String 이외 모두 소문자
+
+
+    }
+}
---
+++ src/VariableEx2.java
@@ -0,0 +1,48 @@
+public class VariableEx2 {
+    // 메인 메서드 (=기능 행동) 프로그램 실행하는 구문
+    public static void main(String[] args) {
+        /* ** 컴퓨터의 값 처리 원칙 **
+            - 같은 자료형끼리 연산, 결과도 같은 자료형
+
+            ** 형변환 **
+
+            - 다른 자료형끼리 연산하는 경우 자료형을 맞게 변환
+
+            ** 자동 형변환 **
+            - 연산 시 값의 범위가 작은 자료형을 값의 범위가 큰 자료형으로 맞추어 변환
+
+        * */
+
+        // 자동 형변환 확인 1
+        int num1 = 9;
+        long num2 = 10_000_000_000L; // 100억
+
+        long result1 = num1 + num2;
+
+        System.out.println("result1 : " + result1);
+        /* int result2 = num1 + num2;
+            필요한 타입 : int
+            제공된 타입 : long
+            자동으로 형변환 되어 result2는 long 값을 지니지만
+            int형 크기의 바구니에 long 크기를 넣으려해서 오류 발생
+
+            데이터를 담는 바구니의 크기보다 데이터가 커서 발생하는 문제
+         */
+
+        // 자동 형변환 확인 2
+        int num3 = 300;
+        double num4 = 12.34;
+        double result2 = num3 + num4;
+        System.out.println("result2 : " + result2); //312,34
+
+        // 자동 형변환 확인 3
+        long num5 = 20L;
+        float num6 = 3.14f;
+        double result3 = num5 + num6;
+        System.out.println("result3 : " + result3);
+        // (참고)
+        // double, float는 실수를 근사치로 표현한 자료형
+        // 소수점문제로 인하여 정확하지 않은 숫자계산 전달
+        // 은행이나 숫자에 민감한 프로젝트에는 사용 지양
+    }
+}
