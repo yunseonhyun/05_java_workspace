717f72594225da8cedf747fad37f8e29f7e0c2db
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-08-26 17:30:11 +0900

    practice.day2 과제

--- src/edu/practice/day2/Practice.java
+++ src/edu/practice/day2/Practice.java
@@ -22,6 +22,36 @@ public class Practice {
         }
     }
 
+
+    /**
+     * 문제 2번
+     */
+
+    public void method2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("높이를 입력하세요 : ");
+        int heigth = sc.nextInt();
+
+        for(int i = 1; i <= heigth; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 문제 3번
+     */
+    public void method3() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("팩토리얼을 구할 숫자를 입력하세요 : ");
+        int number = sc.nextInt();
+
+
+    }
+
+
     /*
     * 문제 4번 : 소수 판별기
     * */
@@ -74,5 +104,51 @@ public class Practice {
                 System.out.println("입력한 숫자보다 정답이 큽니다.");
             }
         }
+    }
+
+
+    /**
+     * 문제 7번 : 문자열 분석기
+     */
+    public void method7() {
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
+        // 위와 같이 클래스 파일을 가져올 때
+        // 파일이 위치한 경로를 매번 앞에 작성하기 번거롭기 때문에
+        // import를 사용하여 패키지 명칭 아래 맨 위에 작성해준다.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("문자열을 숫자 포함해서 입력해보세요 : ");
+        String str = sc.nextLine();
+
+        int totalCount = str.length(); // 사용자가 입력한 문자열의 전체 글자슈룰 세고 변수이름에 저장
+        int letterCount = 0; // 문자열에서 글자의 개수
+        int digitCount = 0; // 문자열에서 숫자의 개수
+        int otherCount = 0; // 문자도 아니고 숫자도 아닌 것의 개수
+
+        for(int i = 0; i < totalCount; i++){
+            char ch = str.charAt(i); // i 번째 문자열을 ch라는 변수이름에 저장
+
+            /** 문자가 숫자인지 글자인지 boolean(불리언) true false 형태로 결과를 확인
+             * Character.isDigit(ch) = ch가 숫자라면 true
+             *      => 숫자가 맞군요!
+             * Character.isLetter(ch) = ch가 문자라면 true
+             *      => 문자가 맞군요!
+             */
+            if(Character.isDigit(ch)){ // ch에 저장된 문자가 숫자라면
+                digitCount++;          // 숫자 개수 +1 추가
+            } else if(Character.isLetter(ch)){ // ch에 저장된 문자가 숫자가 아닌 글자라면
+                letterCount++;                 // 문자 개수 +1 추가
+            } else {
+                otherCount++; // 글자도 아니고 숫자도 아닌 모든 경우 +1 추가
+            }
+
+
+        }
+        System.out.println("\n=== 고객이 작성한 문자열에서 숫자와 문자의 개수 확인하기 ===");
+        System.out.println("총 글자수 : " + totalCount);
+        System.out.println("숫자 아닌 글자수 : " + letterCount);
+        System.out.println("숫자 글자수 : " + digitCount);
+        System.out.println("기타 문자(공백, 기호) : " + otherCount);
+
     }
 }
--- src/edu/practice/day2/PracticeRun.java
+++ src/edu/practice/day2/PracticeRun.java
@@ -4,8 +4,19 @@ public class PracticeRun {
     public static void main(String[] args) {
         Practice ex = new Practice();
         //ex.method1();
-        ex.method4();
+        ex.method2();
+        //ex.method4();
         //ex.method6();
+        //ex.method7();
+        /** practice.method7()
+         * 문자열을 숫자 포함해서 입력해보세요 : 안녕하세요 2025년 8월 입니다.
+         *
+         * === 고객이 작성한 문자열에서 숫자와 문자의 개수 확인하기 ===
+         *      총 글자수 : 19
+         *      숫자 아닌 글자수 : 10
+         *      숫자 글자수 : 5
+         *      공백 마침표는 제외하고 센 숫자값
+         */
 
 
     }
