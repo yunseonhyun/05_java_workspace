05e0a19106ccd95e272ebfb3336a1a327b14f65c
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-09-02 11:18:31 +0900

    상속 - 다형성

--- src/edu/inheritance/ex3/GoogleServiceRun.java
+++ src/edu/inheritance/ex3/GoogleServiceRun.java
@@ -10,17 +10,35 @@ public class GoogleServiceRun {
         // 2. YouTube 객체 생성
         Youtube youTube = new Youtube("user1@gmail.com", "박길동", "개인계정", 50, true);
 
-        System.out.println("=== 구글 서비스 사용자 정보 ===");
-        gmail.showUserInfo();
-        youTube.showUserInfo();
 
-        System.out.println("\n=== 로그인 ===");
-        gmail.login();
-        youTube.login();
+        // 3. 다형성을 활용한 배열 생성
+        // 부모클래스로 자식들을 [] 배열처리해서 리스트로 나열 가능 = 나열될 자식 클래스들
+        // for(부모클래스 하나씩출력할변수이름 : 부모클래스 배열 변수이름) {
+        //              하나씩출력할변수이름.부모클래스기능들();
+        GoogleService[] 서비스들 = {gmail, youTube};
 
-        System.out.println("\n=== 서비스별 검색 ====");
-        gmail.search();
-        youTube.search();
+        // System.out.println("=== 구글 서비스 사용자 정보 ===");
+        // gmail.showUserInfo();
+        // youTube.showUserInfo();
+
+        for(GoogleService 하나씩공통된기능확인하기 :서비스들){
+            System.out.println("=== 구글 서비스 사용자 정보 ===");
+            하나씩공통된기능확인하기.showUserInfo();
+            System.out.println("\n=== 로그인 ===");
+            하나씩공통된기능확인하기.login();
+             System.out.println("\n=== 서비스별 검색 ====");
+            하나씩공통된기능확인하기.search();
+
+        }
+
+
+        //System.out.println("\n=== 로그인 ===");
+        // gmail.login();
+        // youTube.login();
+
+        // System.out.println("\n=== 서비스별 검색 ====");
+        // gmail.search();
+        // youTube.search();
 
         System.out.println("\n=== 서비스별 고유 기능 ===");
         gmail.sendEmail();
