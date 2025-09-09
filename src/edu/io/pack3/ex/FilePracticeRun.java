8053d8d03e931da0e50625b74ae8f03cfd50afce
yunseonhyun <70725625+yunseonhyun@users.noreply.github.com>
2025-09-09 11:47:14 +0900

    IO(입출력)

--- src/edu/io/pack3/ex/FilePracticeRun.java
+++ src/edu/io/pack3/ex/FilePracticeRun.java
@@ -5,10 +5,25 @@ import java.util.Scanner;
 public class FilePracticeRun {
     public static void main(String[] args) {
         FilePracticeService fps = new FilePracticeService();
+        String dirName;
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("폴더를 입력하시겠습니까? (yes/no) : ");
+        //              글자입력받고 공백없음 모두소문자처리
+        String answer = sc.nextLine().trim().toLowerCase();
+        // 없으면 기본프로젝트 최상위 폴더 설정
+        // yes 이면 폴더 입력 후
+        // dirName으로 입력한 폴더이름 method1 전달
+        if(answer.equals("yes")){
+            System.out.print("폴더 이름을 입력해주세요 : ");
+            dirName = sc.nextLine().trim();
+
+        } else {
+            dirName = "";
+            System.out.println("기본 프로젝트 최상위 폴더로 설정됩니다.");
+        }
         System.out.print("내용을 확인할 파일이름 + 확장자를 입력하세요 : ");
         String fileName = sc.nextLine();
-        fps.method1(fileName);
+        fps.method1(dirName, fileName);
     }
 }
--- src/edu/io/pack3/ex/FilePracticeService.java
+++ src/edu/io/pack3/ex/FilePracticeService.java
@@ -13,11 +13,32 @@ public class FilePracticeService {
     // 내부에 intro.txt 파일이 존재하는지 확인
     // 폴더 / 파일 모두 존재하면
     // 현대방식의 파일읽기 방식을 활용해서 intro.txt 내부에 작성된 글자를 모두 읽고 읽은 내용 출력
-    public void method1(String fileName){
-        Path path = Path.of("my_logs", fileName);
+    public void method1(String dirName,String fileName){
+        Path path;
+
+        // 폴더명이 있으면 해당 폴더 안의 파일 확인
+        if(dirName !=null && !dirName.isEmpty()){
+            path = Path.of(dirName,fileName);
+        } else { // 없으면 현재 최상위 프로젝트 폴더 파일에서 검색
+            path = Path.of(dirName);
+        }
+
+        // 폴더의 존재 확인
+        if(path.getParent() !=null) {
+            if(!Files.exists(path.getParent())) {
+                System.out.println("폴더가 존재하지 않습니다. 폴더이름 확인해 주세요.");
+                return;
+            }
+            if(!Files.isDirectory(path)) {
+                System.out.println("폴더가 아니고, 파일 형태 입니다.");
+                return;
+            }
+            System.out.println(path.getParent() + " 가 존재합니다.");
+        }
 
         if(!Files.exists(path)){
             System.out.println("파일이 존재하지 않습니다.");
+            return;
         }
 
         try {
